53ba159f89a81045741421e1feac71325a10d3c0
Leonardo <Leonardo@TMENGN03>
2023-07-01 14:38:04 -0300

    Adicionar arquivos de projeto.

---
+++ projeto_musical/Program.cs
@@ -0,0 +1,17 @@
+﻿musica musica1 = new musica();
+musica1.nome = "Sempre Travado";
+musica1.artista = "Derek";
+musica1.disponivel = true;
+musica1.duracao = 273;
+
+
+
+musica musica2 = new musica();
+musica2.nome = "No Police";
+musica2.artista = "Doja Cat";
+musica2.disponivel = false;
+musica2.duracao = 289;
+
+musica1.ExibirFichaTenica();
+musica2.ExibirFichaTenica();
+musica1.ExbirNomeEArtista();
---
+++ projeto_musical/musica.cs
@@ -0,0 +1,33 @@
+﻿
+//Criação da classe musica e do metodo exibir ficha tecnica
+class musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+
+    //metodo
+    public void ExibirFichaTenica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel == true)
+        {
+            Console.WriteLine($"A musica está disponivel");
+        }
+        else
+        {
+            Console.WriteLine($"Desculpe! a musica não esta disponivel no seu plano");
+        }
+    }
+    public void ExbirNomeEArtista()
+    {
+        Console.WriteLine($"Nome: {nome} - Artista: {artista}");
+    }
+}
+
+
+
